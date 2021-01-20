5c5c68101cf7f4d88889dd02c9b7762851df0b44
Mateus <mateusv@outlook.com.br>
2021-01-20 15:38:59 -0300

    Rotas de entrar e cadastrar criadas!

--- primeiroProjetoAngular/blogPessoalMateus/src/app/app-routing.module.ts
+++ primeiroProjetoAngular/blogPessoalMateus/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //Esse comando está dizendo que ao entrar no site a primeira página será a de login, ou seja, o caminho 'entrar'. O comando pathMatch é para evitar que a página carregue pela metade ou faltando componentes (serve para evitar bugs).
+  {path:'', redirectTo: 'entrar', pathMatch: 'full'},
+  //Rota para a tela de login
+  {path:'entrar', component: EntrarComponent},
+  //Rota para a tela de cadastro
+  {path:'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
