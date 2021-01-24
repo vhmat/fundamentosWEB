f52923833852483a279aca95489d1c502e9f7a0e
Mateus <mateusv@outlook.com.br>
2021-01-24 19:56:40 -0300

    Adicionando parte 1 - 2

--- primeiroProjetoAngular/blogPessoalMateus/src/app/app-routing.module.ts
+++ primeiroProjetoAngular/blogPessoalMateus/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { InicioComponent } from './inicio/inicio.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'entrar', component: EntrarComponent},
+  {path: 'cadastrar', component: CadastrarComponent},
+  {path: 'inicio', component: InicioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- primeiroProjetoAngular/blogPessoalMateus/src/app/entrar/entrar.component.ts
+++ primeiroProjetoAngular/blogPessoalMateus/src/app/entrar/entrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EntrarComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0,0)
   }
 
+  entrar(){
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin)=>{this.userLogin = resp
+    this.router.navigate(['/inicio'])
+    }, erro =>{
+      if(erro.status == 500){
+        alert('Usuário ou senha inválidos!')
+      }
+    })
+    
+  }
 }
