287b35d73204e962c27e8a3e30fe15421fec70df
Mateus <mateusv@outlook.com.br>
2021-01-24 21:29:43 -0300

    Correção de conflitos OK

--- primeiroProjetoAngular/blogPessoalMateus/src/app/model/Postagem.ts
+++ primeiroProjetoAngular/blogPessoalMateus/src/app/model/Postagem.ts
@@ -1,19 +1,12 @@
 import { Tema } from "./Tema"
-<<<<<<< HEAD
 import { Usuario } from "./Usuario"
-=======
 import { User } from "./User"
->>>>>>> consumo-parte1
 
 export class Postagem{
     public id: number
     public titulo: string
     public texto: string
     public data: Date
-<<<<<<< HEAD
     public usuario: Usuario
-=======
-    public usuario: User
->>>>>>> consumo-parte1
     public tema: Tema
 }
