b94e79513b243cdff2f97663da6bf8eb1328af83
Mateus <mateusv@outlook.com.br>
2021-01-25 02:09:58 -0300

    Finalizando o Modal de Nova Postagem

--- primeiroProjetoAngular/blogPessoalMateus/src/app/menu/menu.component.ts
+++ primeiroProjetoAngular/blogPessoalMateus/src/app/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  //Declaração de duas variáveis para receber o que estiver dentro do environment. Dessa forma, podemos chamá-las em outro local para poder exibir o nome e a foto do usuário que estiver logado.
+  nomeUsuario = environment.nome
+  fotoUsuario = environment.foto
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
+  sair(){
+    this.router.navigate(['/entrar'])
+    environment.tolken = ''
+    environment.nome = ''
+    environment.id = 0
+    environment.foto = ''
+  }
 }
--- primeiroProjetoAngular/blogPessoalMateus/src/app/service/auth.service.ts
+++ primeiroProjetoAngular/blogPessoalMateus/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -19,5 +20,15 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('http://localHost:8080/usuarios/cadastrar', user)
+  }
+
+  logado(){
+    //Declarando a variável testandoStatusLogado. 
+    let testandoStatusLogado: boolean = false
+    //Nesse if estamos verificando se existe algum tolken. Se exister, a variável testandoStatusLogado receberá um true.
+    if(environment.tolken != ''){
+      testandoStatusLogado = true
+    }
+    return testandoStatusLogado
   }
 }
